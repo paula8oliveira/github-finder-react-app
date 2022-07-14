4c9eeebec4ac6915fa73068284503a6d58322e9a
Paula Oliveira <poliveira@itx.com>
2022-07-14 16:43:24 -0300

    Styling not found and about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,15 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details.
+        Udemy course by <a href='https://paulaoliveiradev.com'><strong>Paula Oliveira</strong></a>.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+    </>
   )
 }
