751e4bf72bfb4c3d0b44ec5c7b03520d15779d53
Paula Oliveira <poliveira@itx.com>
2022-08-06 17:35:39 -0300

    Add clear search

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -31,6 +31,9 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+    //clear users from state
+    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
+
     //set loading
     const setLoading = () => dispatch({type: 'SET_LOADING'})
 
@@ -40,6 +43,7 @@ export const GithubProvider = ({children}) => {
                 users: state.users,
                 loading: state.loading,
                 searchUsers,
+                clearUsers,
             }}
         >
             {children}
