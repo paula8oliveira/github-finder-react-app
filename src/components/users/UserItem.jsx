a2391f8fb86fc6e3b8967256a70835613bf30887
Paula Oliveira <poliveira@itx.com>
2022-08-07 16:25:53 -0300

    Add user single page

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -14,7 +14,7 @@ export default function UserItem({ user: {login, avatar_url } }) {
             </div>
             <div>
                 <h2 className='card-title'>{login}</h2>
-                <Link className='text-base-content text-opacity-40' to={`/users/${login}`}>Visiti Profile</Link>
+                <Link className='text-base-content text-opacity-40' to={`/user/${login}`}>Visiti Profile</Link>
             </div>
         </div>
     </div>
